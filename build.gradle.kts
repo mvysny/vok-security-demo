a12d57bb7f59ec7b2a13e85946a9479bcd32c75b
Martin Vysny <mavi@vaadin.com>
2020-02-26 09:37:22 +0200

    v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,13 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val vaadinonkotlin_version = "0.8.1"
-val vaadin10_version = "14.1.2"
+val vaadin10_version = "14.1.16"
 
 plugins {
     kotlin("jvm") version "1.3.61"
-    id("org.gretty") version "2.3.1"
+    id("org.gretty") version "3.0.1"
     war
+    id("com.vaadin") version "0.5.0"
 }
 
 defaultTasks("clean", "build")
@@ -39,23 +40,31 @@ tasks.withType<Test> {
 dependencies {
     // Vaadin-on-Kotlin dependency, includes Vaadin
     compile("eu.vaadinonkotlin:vok-framework-v10-vokdb:$vaadinonkotlin_version")
-    compile(enforcedPlatform("com.vaadin:vaadin-bom:$vaadin10_version"))
+    // Vaadin 14
+    compile("com.vaadin:vaadin-core:${properties["vaadin_version"]}") {
+        // Webjars are only needed when running in Vaadin 13 compatibility mode
+        listOf("com.vaadin.webjar", "org.webjars.bowergithub.insites",
+                "org.webjars.bowergithub.polymer", "org.webjars.bowergithub.polymerelements",
+                "org.webjars.bowergithub.vaadin", "org.webjars.bowergithub.webcomponents")
+                .forEach { exclude(group = it) }
+    }
     providedCompile("javax.servlet:javax.servlet-api:3.1.0")
+
     compile("com.zaxxer:HikariCP:3.4.1")
 
     // logging
     // currently we are logging through the SLF4J API to LogBack. See src/main/resources/logback.xml file for the logger configuration
     compile("ch.qos.logback:logback-classic:1.2.3")
-    compile("org.slf4j:slf4j-api:1.7.25")
+    compile("org.slf4j:slf4j-api:1.7.30")
 
     compile(kotlin("stdlib-jdk8"))
 
     // db
-    compile("org.flywaydb:flyway-core:6.1.0")
+    compile("org.flywaydb:flyway-core:6.1.4")
     compile("com.h2database:h2:1.4.200")
 
     // test support
-    testCompile("com.github.mvysny.kaributesting:karibu-testing-v10:1.1.17")
+    testCompile("com.github.mvysny.kaributesting:karibu-testing-v10:1.1.19")
     testCompile("com.github.mvysny.dynatest:dynatest-engine:0.15")
 
     // heroku app runner
