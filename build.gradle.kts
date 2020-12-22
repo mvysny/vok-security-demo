8ca8c87403e4cef2c5d9f9aff89d496aa6fe5e67
Martin Vysny <mavi@vaadin.com>
2020-12-22 09:36:27 +0200

    v-bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,10 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val vaadinonkotlin_version = "0.9.0"
-val vaadin10_version = "14.4.3"
+val vaadin10_version = "14.4.4"
 
 plugins {
-    kotlin("jvm") version "1.4.10"
+    kotlin("jvm") version "1.4.21"
     id("org.gretty") version "3.0.3"
     war
     id("com.vaadin") version "0.14.3.7"
@@ -63,7 +63,7 @@ dependencies {
     implementation("com.h2database:h2:1.4.200")
 
     // test support
-    testImplementation("com.github.mvysny.kaributesting:karibu-testing-v10:1.2.6")
+    testImplementation("com.github.mvysny.kaributesting:karibu-testing-v10:1.2.7")
     testImplementation("com.github.mvysny.dynatest:dynatest-engine:0.19")
 
     // heroku app runner
