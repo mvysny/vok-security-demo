3bb20b5c0ca7e109ca63dbd0c69b35ec9f117b60
Martin Vysny <mavi@vaadin.com>
2022-01-16 13:49:12 +0200

    try to fix failing builds on heroku

--- build.gradle.kts
+++ build.gradle.kts
@@ -91,5 +91,5 @@ vaadin {
     if (gradle.startParameter.taskNames.contains("stage")) {
         productionMode = true
     }
-    nodeVersion = "v16.13.2"
+    nodeVersion = "v16.13.2" // workaround for https://github.com/vaadin/flow/issues/12732
 }
