017e7854a31bfa4a09b2bf6465d951b00ee0380a
Martin Vysny <mavi@vaadin.com>
2022-10-30 16:45:44 +0200

    dep v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val vaadinonkotlin_version = "0.13.0"
+val vaadinonkotlin_version = "0.14.0"
 val vaadin_version = "23.2.6"
 
 plugins {
