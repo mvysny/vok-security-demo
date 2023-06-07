0921f30dd622d03228e5afecf0351b25d6ec32e1
Martin Vysny <mavi@vaadin.com>
2023-06-07 22:54:11 +0300

    vaadin v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,18 +2,17 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val vaadinonkotlin_version = "0.15.0"
-val vaadin_version = "24.1.0.rc1"
+val vaadin_version = "24.1.0"
 
 plugins {
     kotlin("jvm") version "1.8.21"
     id("application")
-    id("com.vaadin") version "24.1.0.rc1"
+    id("com.vaadin") version "24.1.0"
 }
 
 defaultTasks("clean", "build")
 
 repositories {
-    maven { setUrl("https://maven.vaadin.com/vaadin-prereleases") }
     mavenCentral()
 }
 
