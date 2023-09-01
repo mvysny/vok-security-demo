1a4c4d9fa66a224cee4fd0a28f57d27a61b9e794
Martin Vysny <mavi@vaadin.com>
2023-09-01 16:00:48 +0300

    dep v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,8 +57,8 @@ dependencies {
 
     // db
     implementation("com.zaxxer:HikariCP:5.0.1")
-    implementation("org.flywaydb:flyway-core:9.16.0")
-    implementation("com.h2database:h2:2.2.220")
+    implementation("org.flywaydb:flyway-core:9.22.0")
+    implementation("com.h2database:h2:2.2.222")
 
     // test support
     testImplementation("com.github.mvysny.kaributesting:karibu-testing-v24:2.1.0")
