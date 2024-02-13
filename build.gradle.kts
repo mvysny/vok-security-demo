8e9cdf5a9f5269dbf74b67861f974d048c30f51f
Martin Vysny <mavi@vaadin.com>
2024-02-13 17:43:18 +0200

    gradle: move deps to libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,11 @@
+import com.vaadin.gradle.getBooleanProperty
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val vaadinVersion: String by extra
-val vokVersion: String by extra
-val karibuDslVersion: String by extra
-
 plugins {
     kotlin("jvm") version "1.9.22"
-    id("application")
-    id("com.vaadin")
+    application
+    alias(libs.plugins.vaadin)
 }
 
 defaultTasks("clean", "build")
@@ -32,33 +29,31 @@ tasks.withType<Test> {
 
 dependencies {
     // Vaadin-related dependencies
-    implementation("com.vaadin:vaadin-core:$vaadinVersion") {
-        afterEvaluate {
-            if (vaadin.productionMode.get()) {
-                exclude(module = "vaadin-dev")
-            }
+    implementation(libs.vaadin.core) {
+        // https://github.com/vaadin/flow/issues/18572
+        if (vaadin.productionMode.map { v -> getBooleanProperty("vaadin.productionMode") ?: v }.get()) {
+            exclude(module = "vaadin-dev")
         }
     }
-    implementation("eu.vaadinonkotlin:vok-framework-vokdb:$vokVersion") {
-        exclude(group = "com.vaadin")
-    }
-    implementation("com.github.mvysny.vaadin-simple-security:vaadin-simple-security:1.0")
-    implementation("com.github.mvysny.vaadin-boot:vaadin-boot:12.2")
+    implementation(libs.vok.db)
+    implementation(libs.vaadin.security.simple)
+    implementation(libs.vaadin.boot)
 
     // logging
     // currently we are logging through the SLF4J API to SLF4J-Simple. See src/main/resources/simplelogger.properties file for the logger configuration
-    implementation("org.slf4j:slf4j-simple:2.0.9")
+    implementation(libs.slf4j.simple)
 
     implementation(kotlin("stdlib-jdk8"))
 
     // db
-    implementation("com.zaxxer:HikariCP:5.0.1")
-    implementation("org.flywaydb:flyway-core:9.22.3")
-    implementation("com.h2database:h2:2.2.224")
+    implementation(libs.hikaricp)
+    implementation(libs.flyway)
+    implementation(libs.h2)
 
     // test support
-    testImplementation("com.github.mvysny.kaributesting:karibu-testing-v24:2.1.2")
-    testImplementation("com.github.mvysny.dynatest:dynatest:0.24")
+    testImplementation(libs.karibu.testing)
+    testImplementation(libs.dynatest)
+    testRuntimeOnly(libs.junit.platform.launcher)
 }
 
 java {
