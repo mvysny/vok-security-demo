6ae05e4b1ffee0d97631906278f9e2125ac2322f
Martin Vysny <mavi@vaadin.com>
2024-10-19 22:34:05 +0300

    dep v bump

--- src/main/kotlin/com/vaadin/securitydemo/admin/AdminRoute.kt
+++ src/main/kotlin/com/vaadin/securitydemo/admin/AdminRoute.kt
@@ -2,6 +2,7 @@ package com.vaadin.securitydemo.admin
 
 import com.github.mvysny.karibudsl.v10.*
 import com.github.vokorm.exp
+import com.gitlab.mvysny.jdbiorm.condition.Condition
 import com.gitlab.mvysny.jdbiorm.vaadin.filter.FilterTextField
 import com.gitlab.mvysny.jdbiorm.vaadin.filter.NumberRangePopup
 import com.vaadin.flow.component.textfield.TextField
@@ -20,29 +21,51 @@ import jakarta.annotation.security.RolesAllowed
 @PageTitle("Administration")
 @RolesAllowed("ROLE_ADMIN")
 class AdminRoute : KComposite() {
+    private val dataProvider = User.dataProvider
+    private val idFilter = NumberRangePopup()
+    private val usernameFilter = FilterTextField()
+    private val rolesFilter = FilterTextField()
+
     private val root = ui {
         verticalLayout {
             h1("Administration pages")
-            grid<User>(User.dataProvider) {
+            grid<User>(dataProvider) {
                 isExpand = true
 
                 appendHeaderRow() // workaround for https://github.com/vaadin/vaadin-grid-flow/issues/912
-                val filterBar: FilterBar<User> = appendHeaderRow().asFilterBar(this)
+                val filterBar = appendHeaderRow()
 
                 columnFor(User::id) {
                     setSortProperty(User::id.exp)
-                    filterBar.forField(NumberRangePopup(), this).inRange()
+                    idFilter.addValueChangeListener { updateFilter() }
+                    filterBar.getCell(this).component = idFilter
                 }
                 columnFor(User::username) {
                     setSortProperty(User::username.exp)
-                    filterBar.forField(FilterTextField(), this).istartsWith()
+                    usernameFilter.addValueChangeListener { updateFilter() }
+                    filterBar.getCell(this).component = usernameFilter
                 }
                 columnFor(User::roles) {
                     setSortProperty(User::roles.exp)
-                    filterBar.forField(FilterTextField(), this).istartsWith()
+                    rolesFilter.addValueChangeListener { updateFilter() }
+                    filterBar.getCell(this).component = rolesFilter
                 }
                 addColumn { it.hashedPassword } .setHeader("Hashed Password")
             }
         }
+    }
+
+    private fun updateFilter() {
+        var c: Condition = Condition.NO_CONDITION
+        if (!idFilter.isEmpty) {
+            c = c.and(idFilter.value.asLongInterval().contains(User::id.exp))
+        }
+        if (usernameFilter.value.isNotBlank()) {
+            c = c.and(User::username.exp.startsWithIgnoreCase(usernameFilter.value))
+        }
+        if (rolesFilter.value.isNotBlank()) {
+            c = c.and(User::roles.exp.startsWithIgnoreCase(rolesFilter.value))
+        }
+        dataProvider.filter = c
     }
 }
